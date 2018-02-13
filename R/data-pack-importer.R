93f5506be871355737ad6934931104e2a02397f3
Jason Pickering <jason.p.pickering@gmail.com>
2018-02-13 09:30:18 +0100

    Sheets -> sheet_name

--- R/data-pack-importer.R
+++ R/data-pack-importer.R
@@ -1,58 +1,70 @@
 #' @export
-#' @title ValidateSheet(wb_path,sheet_name,wb_type)
+#' @title ValidateSheet(wb_path,schema)
 #'
 #' @description Validates the layout of a single sheet based on its schema definition.
 #' @param wb_path The absolute file path to the workbook.
-#' @param sheet_name The name of the specific sheet to be validated within the workbook.
 #' @param schema Specific schema for the sheet 
 #' @return Returns a boolean value TRUE if the sheet is valid, otherwise, FALSE.
 #'
-ValidateSheet <- function(wb_path, sheet_name,schema) {
+ValidateSheet <- function(wb_path,schema) {
 
   cell_range = readxl::cell_limits(c(schema$row, schema$start_col),
                                    c(schema$row, schema$end_col))
   all( names(
-    readxl::read_excel(wb_path, sheet = sheet_name, range = cell_range)
+    readxl::read_excel(wb_path, sheet = schema$sheet_name, range = cell_range)
   ) == unlist(schema$fields,use.names = FALSE))
   
 }
 
 #' @export
-#' @title GetWorkbookType(wb_path)
+#' @title GetWorkbookInfo(wb_path)
 #'
-#' @description Determines the workbook type
+#' @description Provides information about the workbook
 #' @param wb_path The absolute file path to the workbook.
-#' @return Returns a character value of either NORMAL or HTS. Exits on anything else.
+#' @return Returns a list consiting of :
+#'  \itemize{
+#'    \item wb_path: Full path to the disagg tool 
+#'    \item timestamp: Timestamp of when this script was run
+#'    \item wb_type: Should either be HTS or NORMAL
+#'    \item ou_name: Name of the operating unit
+#'    \item ou_uid: UID of the operating unit }
+#' 
 #'
-GetWorkbookType<-function(wb_path) {
-  foo<-names(readxl::read_excel(wb_path, sheet = "Home", range = "O3"))
-  if (foo == "normal") {
-    return("NORMAL")
-  } else if (foo == "hts") {
-    return("HTS")
+GetWorkbookInfo<-function(wb_path) {
+  wb_type<-names(readxl::read_excel(wb_path, sheet = "Home", range = "O3"))
+  if ( wb_type == "normal") {
+    wb_type = "NORMAL"
+  } else if (wb_type == "hts") {
+    wb_type = "HTS"
   } else
   {
     stop("Unknown workbook type. Must be 'normal' or 'hts'!")
   }
-  
+  ou_uid<-names(readxl::read_excel(wb_path, sheet = "Home", range = "O4"))
+  ou_name<-names(readxl::read_excel(wb_path, sheet = "Home", range = "O1"))
+  return(list(
+    wb_path = tools::file_path_as_absolute(wb_path),
+    timestamp = Sys.time(),
+    wb_type=wb_type,
+    ou_name=ou_name,
+    ou_uid=ou_uid))
   }
 
 #' @export
-#' @title ValidateWorkbook(wb_path,wb_type)
+#' @title ValidateWorkbook(wb_path)
 #'
 #' @description Validates the layout of all relevant sheets in a data pack workbook
 #' @param wb_path  The absolute file path to the workbook.
-#' @param wb_type Should be either NORMAL or HTS
-#' @return Returns a boolean value TRUE if the the workbook is valid, otherwise false
+#' @return Returns a boolean value TRUE if the the workbook is valid, otherwise FALSE
 #'
 #'
 #'
 ValidateWorkbook <- function(wb_path) {
-  wb_type = GetWorkbookType(wb_path)
-  if (wb_type == "HTS") { schemas <- datapackimporter::hts_schema }
-  if (wb_type == "NORMAL") { schemas <-datapackimporter::main_schema }
+  wb_info = GetWorkbookInfo(wb_path)
+  if (wb_info$wb_type == "HTS") { schemas <- datapackimporter::hts_schema }
+  if (wb_info$wb_type == "NORMAL") { schemas <-datapackimporter::main_schema }
   all_tables <- readxl::excel_sheets(path = wb_path)
-  expected <- unlist(sapply(schemas$schema, `[`, c('sheet')),use.names = FALSE)
+  expected <- unlist(sapply(schemas$schema, `[`, c('sheet_name')),use.names = FALSE)
   all_there <- expected %in% all_tables
   #Validate against expected tables
   if ( !all(all_there) ) {
@@ -65,9 +77,9 @@ ValidateWorkbook <- function(wb_path) {
                stringsAsFactors = FALSE)
   for (i in 1:nrow(result)) {
     sheet_name<-as.character(result$sheet_name[i])
-    schema<-rlist::list.find(schemas$schema,sheet==sheet_name)[[1]]
+    schema<-rlist::list.find(schemas$schema,sheet_name==sheet_name)[[1]]
     result$valid[i] <-
-      ValidateSheet(wb_path, sheet_name,schema)
+      ValidateSheet(wb_path,schema)
   }
   
   if (any(!(result$valid))) {
@@ -82,16 +94,22 @@ ValidateWorkbook <- function(wb_path) {
 }
 
 #' @export
-#' @title ImportSheet(wb_path,sheet_name,wb_type)
+#' @importFrom stats complete.cases
+#' @title ImportSheet(wb_path,schema)
 #'
 #' @description Imports a single sheet from a workbook.
 #' @param wb_path  The absolute file path to the workbook.
-#' @param sheet_name Name of the sheet to import. 
-#' @param wb_type Should be either NORMAL or HTS.
-#' @return Returns a data frame with the following columns:
-#' #' \itemize{psnuuid,mechid,type,variable,value
-#'   \item psnuid: Name of the PSNU or cluster
-#'   \item mechid: Mechanism number
+#' @param schema Schema of the sheet
+#' @return Returns a data frame with the following columns. 
+#' Will return an empty data frame if the sheet cannot be processed.
+#' 
+#' #' \itemize{
+#'   \item datalement: UID of the data elememnt
+#'   \item period: ISO string of the period
+#'   \item orgunit: UID of the organisation unit
+#'   \item categoryoptioncombo: UID of the category option combo
+#'   \item attributeoptioncombo: UID of the mechanism
+#'   \item value: Value as a string.
 #' }
 #'
 
@@ -104,9 +122,10 @@ ImportSheet <- function(wb_path, schema) {
   des<-datapackimporter::des
   if ( schema$method == "standard") {
   d <-
-    readxl::read_excel(wb_path, sheet = schema$sheet, range = cell_range) %>%
-    tidyr::gather(variable, value, -c(1:7)) %>% 
-    dplyr::filter(.,  value != 0) %>% 
+    readxl::read_excel(wb_path, sheet = schema$sheet_name, range = cell_range) %>%
+    mutate_all(as.character) %>%
+    tidyr::gather(variable, value, -c(1:7),convert =FALSE) %>% 
+    dplyr::filter(.,  value != "0" ) %>% 
     dplyr::filter(!is.na(value)) %>%
     dplyr::select(.,orgunit= psnuuid,mechid,type,variable,value) %>%
     dplyr::mutate(.,
@@ -127,7 +146,8 @@ ImportSheet <- function(wb_path, schema) {
     to<-c("r4zbW3owX9n","Rom79qVjNVb")
     #https://www.datim.org/api/optionSets/mvbwbgbJgXr.json?fields=options[code,name]
     d <-
-      readxl::read_excel(wb_path, sheet = schema$sheet, range = cell_range) %>%
+      readxl::read_excel(wb_path, sheet = schema$sheet_name, range = cell_range) %>%
+      mutate_all(as.character) %>%
       dplyr::mutate(.,
                     snu_priotization_fy19 =  plyr::mapvalues(snu_priotization_fy19,
                                               datapackimporter::impatt$options$dp_code,
@@ -163,19 +183,19 @@ ImportSheet <- function(wb_path, schema) {
 #'
 #' @description Imports the follow on mechs sheet.
 #' @param wb_path  The absolute file path to the workbook.
-#' @return A data  frame with three columns Closing Out, Follow On, Notes
-#'
+#' @return A data  frame with three columns Closing Out, Follow On, Notes. 
+#' If this sheet is blank, returns NULL.
 
 ImportFollowOnMechs<-function(wb_path) {
-  wb_type = GetWorkbookType(wb_path)
-  if (wb_type == "NORMAL") { schemas <-datapackimporter::main_schema } else {
+  wb_info = GetWorkbookInfo(wb_path)
+  if (wb_info$wb_type == "NORMAL") { schemas <-datapackimporter::main_schema } else {
     stop("Only Normal Disagg tools with follow on mechs are supported!")
   }
   sheet_to_import = "Follow on Mech List"
-  schema<-rlist::list.find(schemas$schema,sheet==sheet_to_import)[[1]]
+  schema<-rlist::list.find(schemas$schema,sheet_name==sheet_to_import)[[1]]
   cell_range = readxl::cell_limits(c(schema$row, schema$start_col),
                                    c(NA, schema$end_col))
-  d<-readxl::read_excel(wb_path, sheet = schema$sheet, range = cell_range)
+  d<-readxl::read_excel(wb_path, sheet = schema$sheet_name, range = cell_range)
   if (!is.null(d) & nrow(d) > 0) {
     return(d)
   } else {
@@ -183,18 +203,25 @@ ImportFollowOnMechs<-function(wb_path) {
   }
 }
 
+
 #' @export
-#' @title ImportSheets(wb_path,wb_type)
+#' @title ImportSheets(wb_path)
 #'
 #' @description Imports all sheets from the workbook
 #' @param wb_path  The absolute file path to the workbook.
-#' @return Returns a list of data frames.
+#' @return Returns a list of data frames: 
+#' #'  \itemize{
+#'            \item wb_info: Workbook Info
+#'            \item data: Standard d2importer data frame
+#'            \item follow_on_mechs: Data frame of follow on mechs.
+#'            }
+
 #'
 ImportSheets <- function(wb_path) {
-  wb_type = GetWorkbookType(wb_path)
-  if (wb_type == "HTS") { schemas <- datapackimporter::hts_schema }
-  if (wb_type == "NORMAL") { schemas <-datapackimporter::main_schema }
-  sheets<-unlist(sapply(schemas$schema, `[`, c('sheet')),use.names = FALSE)
+  wb_info = GetWorkbookInfo(wb_path)
+  if (wb_info$wb_type == "HTS") { schemas <- datapackimporter::hts_schema }
+  if (wb_info$wb_type == "NORMAL") { schemas <-datapackimporter::main_schema }
+  sheets<-unlist(sapply(schemas$schema, `[`, c('sheet_name')),use.names = FALSE)
   df <- tibble::tibble(
     "dataelement" = character(),
     "period" = character(),
@@ -208,21 +235,20 @@ ImportSheets <- function(wb_path) {
   
   for (i in 1:length(sheets_to_import)) {
     
-    schema<-rlist::list.find(schemas$schema,sheet==sheets_to_import[i])[[1]]
+    schema<-rlist::list.find(schemas$schema,sheet_name==sheets_to_import[i])[[1]]
     d <- ImportSheet(wb_path, schema)
     df <- dplyr::bind_rows(df, d)
   }
   
   #Import the follow on mechs
-  if (wb_type == "NORMAL") {
+  if (wb_info$wb_type == "NORMAL") {
   follow_on_mechs<-ImportFollowOnMechs(wb_path)
   } else {
     follow_on_mechs<-NULL
   }
    
     
-  foo <- list(follow_on_mechs=follow_on_mechs,
-              data = df)
-  
-return(foo)
+  return ( list(wb_info = wb_info,
+    follow_on_mechs=follow_on_mechs,
+              data = df) )
 }
