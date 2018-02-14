4a0d4cf3215242c36f9656f274bead043502ef22
Jason Pickering <jason.p.pickering@gmail.com>
2018-02-14 14:13:14 +0100

    Remove unused method.

--- data-raw/produceSchemas.R
+++ data-raw/produceSchemas.R
@@ -102,57 +102,6 @@ processDataElements<-function() {
     dplyr::filter(.,complete.cases(.))
   }
 
-## COPdataElements
-## TODO: Move this to one of the standard views so we 
-## do not need to login to DATIM
-
-GenerateCOPDataElements <- function() {
-  
-  getCOPDataElements <- function(dataset_uid,period,level) {
-    de <- read.csv(url(paste0(getOption("baseurl"),"api/sqlViews/DotdxKrNZxG/data.csv?var=dataSets:",dataset_uid)),stringsAsFactors=FALSE,header=TRUE)
-    de$period <- period
-    de$level <- level
-    de$pdLvl <- paste("pd",period,level,sep="_",collapse=NULL)
-    de$indicator <- str_trim(str_extract(str_replace_all(de$dataelement,"(?<=IMPATT)\\.","_"),"^\\w+\\s"))
-    return(de)
-  }
-  
-  dataset_config<-data.frame(
-  dataset = c(
-    #2019 Targets
-    #(Facility,       Community,        PSNU)
-    "eyI0UOWJnDk",    "l796jk9SW7q",    "msJImKkTJkV",
-    #IMPATT
-    "pTuDWXzkAkJ",
-    #2018 Targets
-    #(Facility,       Community)
-    "AitXBHsC7RA",    "BuRoS9i851o",
-    #2017Q4
-    #(Facility,       Community)
-    "uTvHPA1zqzi",    "O3VMNi8EZSV"),
-  period = c(rep("19_T",3),"IMPT",rep(c("18_T","17_4"), each=2)),
-  level = c("F","C","P","P",rep(c("F","C"),2)))
-  
-  COPdataElements=NULL
-  for (i in 1:length(dataset_config)) {
-    COPdataElements=rbind(COPdataElements,getCOPDataElements(dataset_config$dataset[i],dataset_config$period[i],dataset_config$level[i]))
-  }
-  
-  #TEMPORARY: Code List Fix --> add four lines for PMTCT_ART in PSNU code list which are currently not appearing in sqlView.
-  COPdataElements <- COPdataElements %>%
-    rbind(c("MER Target Setting: PSNU (Facility and Community Combined)","PMTCT_ART (N, DSD, NewExistingArt/Sex/HIVStatus) T_PSNU: ART","PMTCT_ART (N, DSD, NewExistingArt/Sex/HIV) T_PSNU","PMTCT_ART_N_DSD_NewExistingArt_Sex_HIV_T_PSNU","DFdm1fjKS5u","Number of HIV-positive pregnant women who received ART to reduce risk of mother-to-child-transmission during pregnancy.","Life-long ART, New, Female, Positive","Q2EBeMBa8Ga","Q2EBeMBa8Ga","19_T","P","pd_19_T_P","PMTCT_ART")
-          ,c("MER Target Setting: PSNU (Facility and Community Combined)","PMTCT_ART (N, DSD, NewExistingArt/Sex/HIVStatus) T_PSNU: ART","PMTCT_ART (N, DSD, NewExistingArt/Sex/HIV) T_PSNU","PMTCT_ART_N_DSD_NewExistingArt_Sex_HIV_T_PSNU","DFdm1fjKS5u","Number of HIV-positive pregnant women who received ART to reduce risk of mother-to-child-transmission during pregnancy.","Life-long ART, Already, Female, Positive","RTYO8ycjbCt","RTYO8ycjbCt","19_T","P","pd_19_T_P","PMTCT_ART")
-          ,c("MER Target Setting: PSNU (Facility and Community Combined)","PMTCT_ART (N, TA, NewExistingArt/Sex/HIVStatus) T_PSNU: ART", "PMTCT_ART (N, TA, NewExistingArt/Sex/HIV) T_PSNU", "PMTCT_ART_N_TA_NewExistingArt_Sex_HIV_T_PSNU", "w4pjh8fNZx8","Number of HIV-positive pregnant women who received ART to reduce risk of mother-to-child-transmission during pregnancy.","Life-long ART, New, Female, Positive","Q2EBeMBa8Ga","Q2EBeMBa8Ga","19_T","P","pd_19_T_P","PMTCT_ART")
-          ,c("MER Target Setting: PSNU (Facility and Community Combined)","PMTCT_ART (N, TA, NewExistingArt/Sex/HIVStatus) T_PSNU: ART", "PMTCT_ART (N, TA, NewExistingArt/Sex/HIV) T_PSNU", "PMTCT_ART_N_TA_NewExistingArt_Sex_HIV_T_PSNU", "w4pjh8fNZx8","Number of HIV-positive pregnant women who received ART to reduce risk of mother-to-child-transmission during pregnancy.","Life-long ART, Already, Female, Positive","RTYO8ycjbCt","RTYO8ycjbCt","19_T","P","pd_19_T_P","PMTCT_ART")
-    ) %>%
-    unique() %>% 
-    subset(.,COPdataElements$indicator %in% c("TX_RET","TX_NEW","TX_CURR","TX_PVLS","PMTCT_STAT","PMTCT_ART","PMTCT_EID","TB_STAT","TB_ART","TB_PREV","TX_TB","HTS_TST","VMMC_CIRC","HTS_SELF","OVC_SERV","OVC_HIVSTAT","KP_PREV","PP_PREV","KP_MAT","GEND_GBV","GEND_GBV_PEP","PrEP_NEW","IMPATT_PLHIV"))
-
-  return(COPdataElements)
-  
-}
-
-
 
 ##Procedural logic to generate the actual schemas
 ##HTS Template
