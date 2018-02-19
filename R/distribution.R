cb84137c7e385de5d34a3d26708cf9f21f0e68a2
Jason Pickering <jason.p.pickering@gmail.com>
2018-02-19 14:17:45 +0200

    Factor out production of Pct distributions into data-raw directory.

--- R/distribution.R
+++ R/distribution.R
@@ -1,83 +1,9 @@
-
-#' @export
-#' @title distrSource()
-#'
-#' @description Converts FY17/18 dataElements to FY19 PSNU level elements within DATIM extract
-#' @param df Name of dataframe storing DATIM extract.
-#' @param FY FiscalYear for which source distribution should be pulled.
-#' @return Returns a dataframe ready for use in preparing source files.
-#'
-distrSource <- function(df,FY=NULL) {
-    
-    clusterMap<-datapackimporter::clusters
-    rCOP18deMap<-datapackimporter::rCOP18deMap
-    
-    ds <- df %>%
-      dplyr::filter(FiscalYear==FY) %>%
-      dplyr::filter(Value !=0) %>%
-      dplyr::filter(!is.na(Value)) %>%
-      dplyr::filter(PSNUuid != "") %>%
-        #Remove "Sensitive Site" data
-      dplyr::filter(!orgUnit %in% datapackimporter::sites_exclude) %>%
-        #Create COPuid for easy re-mapping
-      dplyr::mutate(pd_2017_2018_S=paste(dataElement,categoryOptionCombo,sep=".",collapse=NULL)) %>%
-        #Pull in FY2019 PSNU-level dataElement and categoryOptionCombo codes
-        #TODO: Redothis with inner_join
-            merge(rCOP18deMap[rCOP18deMap$FiscalYear==FY,c("pd_2017_2018_S","pd_2019_P","FiscalYear")],by=c("pd_2017_2018_S")) %>%
-      dplyr::select(-FiscalYear.y,-FiscalYear.x,-dataElement,-categoryOptionCombo) %>%
-        #Constrain to only those data values entered against dataElements mappable to FY19
-            filter(!is.na(pd_2019_P)) %>%
-        #Create new dataElement and categoryOptionCombo codes
-            mutate(dataElement=str_extract(pd_2019_P,"^(.)+(?=(\\.))"),
-                   categoryOptionCombo=str_extract(pd_2019_P,"(?<=(\\.))(.)+$")) %>%
-            select(-pd_2019_P,-pd_2017_2018_S) %>%
-            select(PSNUuid,attributeOptionCombo,dataElement,categoryOptionCombo,orgUnit,Value)
-    
-    return(ds)
-}
-
-
-
-#' @export
-#' @title siteDistribution()
-#'
-#' @description Creates site-level percentages against which Data Pack values can be multiplied for distribution purposes
-#' @param df Name of distribution source file.
-#' @return Returns a dataframe ready for use in distributing site level targets.
-#'
-
-siteDistribution<-function(df) {
-    
-    clusterMap<-datapackimporter::clusters
-    
-    siteDistr <- df %>%
-        #Remove all dedupe data
-            filter(!attributeOptionCombo %in% c("YGT1o7UxfFu","X8hrDf6bLDC")) %>%
-        #Pull in ClusterUIDs
-            left_join(clusterMap[,c("psnuuid","cluster_psnuuid")], by=c("PSNUuid"="psnuuid")) %>%
-            mutate(dpOrgUnit=case_when(is.na(cluster_psnuuid)~PSNUuid
-                                       ,TRUE~cluster_psnuuid)) %>%
-            select(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo,orgUnit,Value) %>%
-        #Form percentage distributions across site within PSNU/Cluster x IM x DE.COC combination
-            group_by(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo) %>%
-            mutate(sitePct = (Value)/sum(Value)) %>%
-        #Create id to join with Data Pack dataset
-            mutate(whereWhoWhatHuh=paste(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo,sep=".")) %>%
-        #Reorder columns; drop Value, keep percentages
-            ungroup() %>%
-            select(whereWhoWhatHuh,orgUnit,sitePct)
-    
-    return(siteDistr)
-    
-}
-
-
 #' @export
 #' @title distributeCluster()
 #'
 #' @description Distributes clustered Data Pack data to PSNU level for DATIM import
 #' @param df Name of dataframe storing Data Pack data.
-#' @param dm Distribution Method selection, whether based on FY17, FY18, or not distributed
+#' @param distribution_year Distribution Method selection, whether based on FY17. Otherwise, FY18.
 #' @return Returns a dataframe structured for DATIM import.
 #'
 
@@ -135,23 +61,38 @@ distributeCluster <- function(df,distribution_year) {
 #'
 #' @description Distributes Data Pack data (both Clustered and non-Clustered) to Site level for DATIM import
 #' @param df Name of dataframe storing Data Pack data.
-#' @param dm Distribution Method selection, whether based on FY17, FY18, or not distributed
+#' @param distribution_year Distribution Method selection, whether based on FY17 or FY18. 
 #' @return Returns a dataframe structured for DATIM import.
 #'
 
-distributeSite <- function(d,distributionMethod) {
+distributeSite <- function(df,distribution_year) {
     
-    if(distributionMethod)==2017)  { Pcts<-distrSiteFY17 }
-    if(distributionMethod)==2018) Pcts<-distrSiteFY18
-    if(distributionMethod)==0000) stop("User selected not to distribute to site!")
-    
+  
+  #Default distribution is 2018 if not otherwise specified
+  if (distribution_year == 2017) {
+    file_name = "distrSiteFY17.rda"
+  } else if (distribution_year ==  2018) {
+    file_name = "distrSiteFY18.rda"
+  } else
+  {
+    stop("Distribution year must either 2017 or 2018! ")
+  }
+  
+  file_path = paste0(getOption("datapack_distros"), file_name)
+  
+  if (!file.exists(file_path)) {
+    stop(paste("Distribution file could not be found. Please check it exists at",file_path))
+  }
+  
+  Pcts<-readRDS(file = file_path )
+
     ds <- df %>%
         #Create id to link to percent distributions
             mutate(whereWhoWhatHuh=paste(orgunit,attributeoptioncombo,dataelement,categoryoptioncombo,sep=".")) %>%
         #Pull in distribution percentages
         #@Sjackson: What is the point of a left join here? Anything which is NA will have to be dropped
         #And cannot be imported. 
-            inner_join(Pcts,by=c("whereWhoWhatHuh")) %>%
+            left_join(Pcts,by=c("whereWhoWhatHuh")) %>%
        #Do we need to round or what here?
             mutate(value=as.character(floor(as.numeric(value)*sitePct))) %>%
             filter(value != "0") %>% 
--- data-raw/produceDistribution.R
+++ data-raw/produceDistribution.R
@@ -17,6 +17,80 @@
     rm(siteExport)
 
 
+    
+    #' @export
+    #' @title distrSource()
+    #'
+    #' @description Converts FY17/18 dataElements to FY19 PSNU level elements within DATIM extract
+    #' @param df Name of dataframe storing DATIM extract.
+    #' @param FY FiscalYear for which source distribution should be pulled.
+    #' @return Returns a dataframe ready for use in preparing source files.
+    #'
+    distrSource <- function(df,FY=NULL) {
+      
+      clusterMap<-datapackimporter::clusters
+      rCOP18deMap<-datapackimporter::rCOP18deMap
+      
+      ds <- df %>%
+        dplyr::filter(FiscalYear==FY) %>%
+        dplyr::filter(Value !=0) %>%
+        dplyr::filter(!is.na(Value)) %>%
+        dplyr::filter(PSNUuid != "") %>%
+        #Remove "Sensitive Site" data
+        dplyr::filter(!orgUnit %in% datapackimporter::sites_exclude) %>%
+        #Create COPuid for easy re-mapping
+        dplyr::mutate(pd_2017_2018_S=paste(dataElement,categoryOptionCombo,sep=".",collapse=NULL)) %>%
+        #Pull in FY2019 PSNU-level dataElement and categoryOptionCombo codes
+        #TODO: Redothis with inner_join
+        merge(rCOP18deMap[rCOP18deMap$FiscalYear==FY,c("pd_2017_2018_S","pd_2019_P","FiscalYear")],by=c("pd_2017_2018_S")) %>%
+        dplyr::select(-FiscalYear.y,-FiscalYear.x,-dataElement,-categoryOptionCombo) %>%
+        #Constrain to only those data values entered against dataElements mappable to FY19
+        filter(!is.na(pd_2019_P)) %>%
+        #Create new dataElement and categoryOptionCombo codes
+        mutate(dataElement=str_extract(pd_2019_P,"^(.)+(?=(\\.))"),
+               categoryOptionCombo=str_extract(pd_2019_P,"(?<=(\\.))(.)+$")) %>%
+        select(-pd_2019_P,-pd_2017_2018_S) %>%
+        select(PSNUuid,attributeOptionCombo,dataElement,categoryOptionCombo,orgUnit,Value)
+      
+      return(ds)
+    }
+    
+    
+    
+    #' @export
+    #' @title siteDistribution()
+    #'
+    #' @description Creates site-level percentages against which Data Pack values can be multiplied for distribution purposes
+    #' @param df Name of distribution source file.
+    #' @return Returns a dataframe ready for use in distributing site level targets.
+    #'
+    
+    siteDistribution<-function(df) {
+      
+      clusterMap<-datapackimporter::clusters
+      
+      siteDistr <- df %>%
+        #Remove all dedupe data
+        filter(!attributeOptionCombo %in% c("YGT1o7UxfFu","X8hrDf6bLDC")) %>%
+        #Pull in ClusterUIDs
+        left_join(clusterMap[,c("psnuuid","cluster_psnuuid")], by=c("PSNUuid"="psnuuid")) %>%
+        mutate(dpOrgUnit=case_when(is.na(cluster_psnuuid)~PSNUuid
+                                   ,TRUE~cluster_psnuuid)) %>%
+        select(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo,orgUnit,Value) %>%
+        #Form percentage distributions across site within PSNU/Cluster x IM x DE.COC combination
+        group_by(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo) %>%
+        mutate(sitePct = (Value)/sum(Value)) %>%
+        #Create id to join with Data Pack dataset
+        mutate(whereWhoWhatHuh=paste(dpOrgUnit,attributeOptionCombo,dataElement,categoryOptionCombo,sep=".")) %>%
+        #Reorder columns; drop Value, keep percentages
+        ungroup() %>%
+        select(whereWhoWhatHuh,orgUnit,sitePct)
+      
+      return(siteDistr)
+      
+    }
+    
+    
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #II. PREPARE SITE-LEVEL DISTRIBUTION MATRICES####
 #////////////////////////////////////////////////////
@@ -41,3 +115,26 @@ clusterDistribution(distrSourceFY17) %>%
 clusterDistribution(distrSourceFY18) %>%
     saveRDS(paste0(sourceFolder,"distrClusterFY18.rda"))
 
+library(feather)
+library(dplyr)
+
+source_dir<-"/home/jason/consultancy/datim/datapack/"
+setwd(source_dir)
+s_17<-readRDS(file="distrSiteFY17.rda")%>%
+  mutate(year = 2017,
+         mode = "s")
+
+col_names<-names(s_17)
+
+s_18<-readRDS(file="/home/jason/consultancy/datim/datapack/distrSiteFY18.rda") %>%
+  mutate(year = 2018,
+         mode = "s")
+
+c_17<-readRDS(file="/home/jason/consultancy/datim/datapack/distrClusterFY17.rda") %>%
+  mutate(year = 2017,
+         mode = "c") %>% `names<-`(.,col_names)
+c_18<-readRDS(file="/home/jason/consultancy/datim/datapack/distrClusterFY18.rda") %>%
+  mutate(year = 2017,
+         mode = "c") %>% `names<-`(.,col_names)
+
+write_feather(pct_distr1,"/home/jason/consultancy/datim/datapack/pct_distr.feather")
