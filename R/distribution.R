5e3d702e6e93a0cfd378ba566bd067ed9e6df294
Jason Pickering <jason.p.pickering@gmail.com>
2018-02-21 14:08:12 +0200

    Clean up distribution file.

--- R/distribution.R
+++ R/distribution.R
@@ -21,15 +21,14 @@ distributeCluster <- function(df,distribution_year) {
     } else {
       file_name = "distrClusterFY18.rda"
     }
-distributeCluster <- function(DataPack,dm) {
-    
+  
     file_path = paste0(getOption("datapack_distros"), file_name)
     
     if (!file.exists(file_path)) {
       stop(paste("Distribution file could not be found. Please check it exists at",file_path))
     }
     
-    Pcts<-readRDS(file = file_path )
+    Pcts<-readRDS( file = file_path )
     clusterMap<-datapackimporter::clusters
     militaryUnits<-datapackimporter::militaryUnits
     
@@ -54,18 +53,19 @@ distributeCluster <- function(DataPack,dm) {
         dplyr::mutate(orgunit=PSNUuid) %>%
         dplyr::select(dataelement,period,PSNUuid,orgunit,categoryoptioncombo,attributeoptioncombo,value) %>%
         rbind(df[!df$orgunit %in% unique(clusterMap$cluster_psnuuid),])
-    ds <- DataPack %>%
-        filter(orgunit %in% unique(clusterMap$cluster_psnuuid)
-               & !orgunit %in% militaryUnits$orgUnit) %>%
-        mutate(whereWhoWhatHuh=paste(orgunit,attributeoptioncombo,dataelement,categoryoptioncombo,sep=".")) %>%
-        left_join(Pcts,by=c("whereWhoWhatHuh")) %>%
-        #Where there is no history at PSNU level, simply distribute evenly among all underlying PSNUs
-            left_join(clusterAvgs,by=c("orgunit"="cluster_psnuuid")) %>%
-            mutate(Value=case_when(is.na(psnuPct)~Value*avg
-                                  ,TRUE~Value*psnuPct)) %>%
-        mutate(orgunit=PSNUuid) %>%
-        select(dataelement,period,orgunit,categoryoptioncombo,attributeoptioncombo,Value) %>%
-        rbind(DataPack[!DataPack$orgunit %in% unique(clusterMap$cluster_psnuuid),],DataPack[orgunit %in% militaryUnits$orgUnit,])
+    
+        # ds <- DataPack %>%
+        # filter(orgunit %in% unique(clusterMap$cluster_psnuuid)
+        #        & !orgunit %in% militaryUnits$orgUnit) %>%
+        # mutate(whereWhoWhatHuh=paste(orgunit,attributeoptioncombo,dataelement,categoryoptioncombo,sep=".")) %>%
+        # left_join(Pcts,by=c("whereWhoWhatHuh")) %>%
+        # #Where there is no history at PSNU level, simply distribute evenly among all underlying PSNUs
+        #     left_join(clusterAvgs,by=c("orgunit"="cluster_psnuuid")) %>%
+        #     mutate(Value=case_when(is.na(psnuPct)~Value*avg
+        #                           ,TRUE~Value*psnuPct)) %>%
+        # mutate(orgunit=PSNUuid) %>%
+        # select(dataelement,period,orgunit,categoryoptioncombo,attributeoptioncombo,Value) %>%
+        # rbind(DataPack[!DataPack$orgunit %in% unique(clusterMap$cluster_psnuuid),],DataPack[orgunit %in% militaryUnits$orgUnit,])
         
     
     return(ds)
@@ -83,7 +83,7 @@ distributeCluster <- function(DataPack,dm) {
 
 distributeSite <- function(df,distribution_year) {
     
-  
+  militaryUnits<-datapackimporter::militaryUnits
   #Default distribution is 2018 if not otherwise specified
   if (distribution_year == 2017) {
     file_name = "distrSiteFY17.rda"
@@ -112,25 +112,6 @@ distributeSite <- function(df,distribution_year) {
         dplyr::filter(value != "0") %>% 
       #Don't we have to remap back to the Site level data elements from the PSNU data elements?
         dplyr::select(dataelement,period,orgunit=orgUnit,categoryoptioncombo,attributeoptioncombo,value)
-distributeSite <- function(DataPack,dm) {
-    
-    if(dm==2017) Pcts<-distrSiteFY17
-    if(dm==2018) Pcts<-distrSiteFY18
-    if(dm==0000) stop("User selected not to distribute to site!")
-    
-    militaryUnits<-datapackimporter::militaryUnits
-    
-    ds <- DataPack %>%
-        #Create id to link to percent distributions
-            mutate(whereWhoWhatHuh=paste(orgunit,attributeoptioncombo,dataelement,categoryoptioncombo,sep=".")) %>%
-        #Pull in distribution percentages
-            left_join(Pcts,by=c("whereWhoWhatHuh")) %>%
-            mutate(Value=Value*sitePct) %>%
-            #rbind(DataPack[orgunit %in% militaryUnits$orgUnit,]) %>%
-            mutate(PSNUuid=orgunit
-                   ,orgunit=orgUnit) %>%
-        select(dataelement,period,PSNUuid,orgunit,categoryoptioncombo,attributeoptioncombo,Value)
-        
-    
+
     return(ds)
 }
