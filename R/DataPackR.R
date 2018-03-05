c73e4b027c9a8816ce24dddf2c49bf040aa27041
Jason Pickering <jason.p.pickering@gmail.com>
2018-03-05 13:54:17 +0100

    Site tool fixes.

--- R/DataPackR.R
+++ R/DataPackR.R
@@ -8,23 +8,23 @@
 #'
 #'
 DataPackR <- function(wb_path, distribution_method, support_files_path) {
-  parsed_data<-
-    ImportSheets(wb_path,
-                 distribution_method = distribution_method,
-                 support_files_path = support_files_path)
-  if (parsed_data$wb_info$wb_type %in% c("NORMAL","HTS")) {
+  parsed_data <-
+    ImportSheets(
+      wb_path,
+      distribution_method = distribution_method,
+      support_files_path = support_files_path
+    )
+  if (parsed_data$wb_info$wb_type %in% c("NORMAL", "HTS")) {
     print("Found a disagg tool!")
     prepare_export_to_datim(parsed_data)
-    #Distribute the site level data
-    site_data<-distributeSite(parsed_data)
-    #Create the site level tool
+    # Distribute the site level data
+    site_data <- distributeSite(parsed_data)
+    # Create the site level tool
     export_site_level_tool(site_data)
-  } else if (parsed_data$wb_info$wb_type %in% c("NORMAL_SITE","HTS_SITE")) {
+  } else if (parsed_data$wb_info$wb_type %in% c("NORMAL_SITE", "HTS_SITE")) {
     print("Found a site level tool!")
     prepare_export_to_datim(parsed_data)
   } else {
     stop("Looks like a bad DataPack!")
   }
-
-
 }
