9a7a864566d84be172afc003fd4ea172b99b771b
Scott <scott.j.jackson@gmail.com>
2018-03-06 02:43:00 -0500

    Map follow-on mechs

--- R/distribution.R
+++ R/distribution.R
@@ -55,7 +55,11 @@ distributeCluster <- function(d) {
       stop(paste("Distribution file could not be found. Please check it exists at", file_path))
     }
 
-    Pcts <- readRDS(file = file_path)
+    Pcts <- readRDS(file = file_path) %>%
+        dplyr::filter(uidlevel3 == d$wb_info$ou_uid)
+    
+    
+    
     clusterMap <- datapackimporter::clusters
     militaryUnits <- datapackimporter::militaryUnits
     ou_uid <- d$wb_info$ou_uid
@@ -154,8 +158,22 @@ distributeSite <- function(d) {
     stop(paste("Distribution file could not be found. Please check it exists at", file_path))
   }
 
+  followOns <- d$follow_on_mechs %>%
+      dplyr::mutate(closingCode=as.character(`Closing Out`),followOnCode=as.character(`Follow on`)) %>%
+      dplyr::left_join(mechs,by=c("closingCode"="code")) %>%
+      dplyr::select(closingCode,closingUID=uid,followOnCode) %>%
+      dplyr::left_join(mechs,by=c("followOnCode"="code")) %>%
+      dplyr::select(closingCode,closingUID,followOnCode,followOnUID=uid)
+  
   Pcts <- readRDS(file = file_path) %>%
-    dplyr::filter(uidlevel3 == d$wb_info$ou_uid)
+    dplyr::filter(uidlevel3 == d$wb_info$ou_uid) %>%
+    #Map follow-on mechs
+        dplyr::mutate(attributeoptioncombo=stringr::str_extract(whereWhoWhatHuh,"(?<=(^\\w{11}\\.))\\w{11}")) %>%
+        dplyr::left_join(select(followOns,closingUID,followOnUID),by=c("attributeoptioncombo"="closingUID")) %>%
+        dplyr::mutate(whereWhoWhatHuh=dplyr::case_when(!is.na(followOnUID)~stringr::str_replace(whereWhoWhatHuh,attributeoptioncombo,followOnUID),
+                                                       TRUE~whereWhoWhatHuh)) %>%
+        dplyr::select(-attributeoptioncombo,-followOnUID)
+    
 
   de_map <- datapackimporter::rCOP18deMapT %>%
     dplyr::select(supportType, pd_2019_S, pd_2019_P, DataPackCode) %>%
