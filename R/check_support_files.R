98092adeb0277bcbc6a8b8adaa0e476ae7bddc8c
Jason Pickering <jason.p.pickering@gmail.com>
2018-03-10 12:14:28 +0100

    Allow a paramater to specify an alternative support files director for checking.

--- R/check_support_files.R
+++ R/check_support_files.R
@@ -5,10 +5,15 @@
 #' @description Checks DataPack support files to ensure they are compatible
 #' with this version of the code.
 #' @param support_files_path  The absolute file path to support files directory.
+#' @param want_md5s A named list of support files along with their MD5 signature
 #' @return Returns a list of files which are not correct.
 #'
-check_support_files <- function(support_files_path) {
-  want_md5s <- datapackimporter::support_files_md5
+check_support_files <- function(support_files_path,want_md5s=NA) {
+  
+  if (is.na(want_md5s)){
+    want_md5s <- datapackimporter::support_files_md5   
+  } 
+ 
   support_files <- paste0(support_files_path, names(want_md5s))
   foo <- as.list(tools::md5sum(support_files))
   names(foo) <- basename(names(foo))
